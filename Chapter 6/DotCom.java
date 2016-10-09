1a840c889a256a2aeb95a8be401c738317bd2891
Vivek Shukla <viv3kshukla@gmail.com>
2016-10-09 22:59:35 +0530

    chapter 6 programs added

---
+++ Chapter 6/DotCom.java
@@ -0,0 +1,33 @@
+import java.util.ArrayList;
+
+class DotCom
+{
+	private ArrayList<String> locationCells;
+	private String name;
+
+	public void setLocationCells(ArrayList<String> loc)
+	{
+		locationCells = loc;
+	}
+
+	public void setName(String n)
+	{
+		name = n;
+	}
+
+	public String checkYourself(String userInput)
+	{
+		String result = "miss";
+
+		int index = locationCells.indexOf(userInput);
+		if (index >= 0)
+		{
+			locationCells.remove(index);
+			if (locationCells.isEmpty())
+				result = "kill";
+			else
+				result = "hit";
+		}
+		return result;
+	}
+}
