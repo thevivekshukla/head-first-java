224d52e16906d9cc225c2d7520624f83529903c4
Vivek Shukla <viv3kshukla@gmail.com>
2016-10-21 17:57:10 +0530

    added QuizCardPlayer and QuizCardBuilder files

---
+++ Chapter 14/ReadAFile.java
@@ -0,0 +1,24 @@
+import java.io.*;
+
+public class ReadAFile
+{
+	public static void main(String[] args)
+	{
+		try
+		{
+			File myFile = new File("Foo.txt");
+			FileReader fileReader = new FileReader(myFile);
+			BufferedReader reader = new BufferedReader(fileReader);
+
+			String line = null;
+			while ((line=reader.readLine())!=null)
+			{
+				System.out.println(line);
+			}
+		}
+		catch(Exception ex)
+		{
+			ex.printStackTrace();
+		}
+	}
+}
---
+++ Chapter 14/WriteAFile.java
@@ -0,0 +1,18 @@
+import java.io.*;
+
+public class WriteAFile
+{
+	public static void main(String[] args)
+	{
+		try
+		{
+			FileWriter writer = new FileWriter("Foo.txt");
+			writer.write("hello foo! file");
+			writer.close();
+		}
+		catch(IOException ex)
+		{
+			ex.printStackTrace();
+		}
+	}
+}
